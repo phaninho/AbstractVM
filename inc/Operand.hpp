ef368c5805b189a02f118691dd4cfed42999913c
phaninho <phaninho@live.fr>
2018-04-08 04:22:48 +0200

    update operand.hpp

--- inc/Operand.hpp
+++ inc/Operand.hpp
@@ -6,7 +6,7 @@
 /*   By: stmartin <stmartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 15:23:19 by stmartin          #+#    #+#             */
-/*   Updated: 2018/04/07 16:12:58 by stmartin         ###   ########.fr       */
+/*   Updated: 2018/04/08 04:22:01 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ public:
 
 	virtual int getPrecision( void ) const; // Precision of the type of the instance
 	virtual eOperandType getType( void ) const; // Type of the instance
-	virtual IOperand const * operator+( Operand const & rhs ) const; // Sum
-	virtual IOperand const * operator-( Operand const & rhs ) const; // Difference
-	virtual IOperand const * operator*( Operand const & rhs ) const; // Product
-	virtual IOperand const * operator/( Operand const & rhs ) const; // Quotient
-	virtual IOperand const * operator%( Operand const & rhs ) const; // Modulo
+	virtual IOperand const * operator+( IOperand const & rhs ) const; // Sum
+	virtual IOperand const * operator-( IOperand const & rhs ) const; // Difference
+	virtual IOperand const * operator*( IOperand const & rhs ) const; // Product
+	virtual IOperand const * operator/( IOperand const & rhs ) const; // Quotient
+	virtual IOperand const * operator%( IOperand const & rhs ) const; // Modulo
 
  	virtual std::string const & toString( void ) const; // String representation of the instance
 };
