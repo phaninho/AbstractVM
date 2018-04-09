6e576ad2a303253ab7b30692f0e373e13f9dc950
phaninho <phaninho@live.fr>
2018-04-09 11:56:33 +0200

    [update] operand.cpp

--- inc/Operand.hpp
+++ inc/Operand.hpp
@@ -6,7 +6,7 @@
 /*   By: stmartin <stmartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 15:23:19 by stmartin          #+#    #+#             */
-/*   Updated: 2018/04/09 11:50:31 by stmartin         ###   ########.fr       */
+/*   Updated: 2018/04/09 11:55:58 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,70 @@ class Operand : public IOperand {
 
 public:
 
-	Operand( void );
-	Operand( Operand const & );
-	virtual ~Operand( void );
+	Operand( void )
+	{
 
-	virtual int getPrecision( void ) const; // Precision of the type of the instance
-	virtual eOperandType getType( void ) const; // Type of the instance
+	}
 
-	virtual IOperand const * operator+( IOperand const & rhs ) const; // Sum
-	virtual IOperand const * operator-( IOperand const & rhs ) const; // Difference
-	virtual IOperand const * operator*( IOperand const & rhs ) const; // Product
-	virtual IOperand const * operator/( IOperand const & rhs ) const; // Quotient
-	virtual IOperand const * operator%( IOperand const & rhs ) const; // Modulo
+	Operand( Operand const & )
+	{
 
- 	virtual std::string const & toString( void ) const; // String representation of the instance
+	}
+
+	virtual ~Operand( void )
+	{
+
+	}
+	
+
+	virtual int getPrecision( void ) const // Precision of the type of the instance
+	{
+
+	}
+
+	virtual eOperandType getType( void ) const // Type of the instance
+	{
+
+	}
+
+
+	virtual IOperand const * operator+( IOperand const & rhs ) const // Sum
+	{
+
+	}
+
+	virtual IOperand const * operator-( IOperand const & rhs ) const // Difference
+	{
+
+	}
+
+	virtual IOperand const * operator*( IOperand const & rhs ) const // Product
+	{
+
+	}
+
+	virtual IOperand const * operator/( IOperand const & rhs ) const // Quotient
+	{
+
+	}
+
+	virtual IOperand const * operator%( IOperand const & rhs ) const // Modulo
+	{
+
+	}
+
+
+ 	virtual std::string const & toString( void ) const // String representation of the instance
+	{
+
+	}
+
 
 private:
 
 	T					_tpl;
 	eOperandType		_type;
-	
+
 };
 
 #endif
