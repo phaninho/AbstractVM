ecd7dfd3f471aa66619277fadf46779afd59a631
phaninho <phaninho@live.fr>
2018-04-09 11:54:13 +0200

    [update] operand.cpp

--- inc/Operand.hpp
+++ inc/Operand.hpp
@@ -6,13 +6,14 @@
 /*   By: stmartin <stmartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 15:23:19 by stmartin          #+#    #+#             */
-/*   Updated: 2018/04/09 01:31:58 by stmartin         ###   ########.fr       */
+/*   Updated: 2018/04/09 11:50:31 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OPERAND_HPP
 #define OPERAND_HPP
 
+template <typename T>
 class Operand : public IOperand {
 
 public:
@@ -23,7 +24,7 @@ public:
 
 	virtual int getPrecision( void ) const; // Precision of the type of the instance
 	virtual eOperandType getType( void ) const; // Type of the instance
-	
+
 	virtual IOperand const * operator+( IOperand const & rhs ) const; // Sum
 	virtual IOperand const * operator-( IOperand const & rhs ) const; // Difference
 	virtual IOperand const * operator*( IOperand const & rhs ) const; // Product
@@ -31,6 +32,12 @@ public:
 	virtual IOperand const * operator%( IOperand const & rhs ) const; // Modulo
 
  	virtual std::string const & toString( void ) const; // String representation of the instance
+
+private:
+
+	T					_tpl;
+	eOperandType		_type;
+	
 };
 
 #endif
--- src/Operand.cpp
+++ src/Operand.cpp
@@ -6,7 +6,7 @@
 /*   By: stmartin <stmartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 14:00:35 by stmartin          #+#    #+#             */
-/*   Updated: 2018/04/07 17:06:12 by stmartin         ###   ########.fr       */
+/*   Updated: 2018/04/09 11:45:08 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,35 @@ Operand::~Operand()
 { }
 
 virtual int getPrecision( void ) const
+{
+
+}
+
 virtual eOperandType getType( void ) const
+{
+	
+}
 
 virtual IOperand const * operator+( Operand const & rhs ) const // Sum
 {
 
 }
 virtual IOperand const * operator-( Operand const & rhs ) const // Difference
+{
+
+}
+
 virtual IOperand const * operator*( Operand const & rhs ) const // Product
+{
+
+}
+
 virtual IOperand const * operator/( Operand const & rhs ) const // Quotient
+{
+
+}
+
 virtual IOperand const * operator%( Operand const & rhs ) const // Modulo
+{
+
+}
